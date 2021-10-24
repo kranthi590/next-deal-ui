85727d2e0ab5a80820b7137a9b8e591fb7d81a95
Anji <anji@Kranthis-MacBook-Pro.local>
2021-10-24 12:17:21 -0300

    Login Integration WIP

--- routes/userAuth/SignIn/index.js
+++ routes/userAuth/SignIn/index.js
@@ -51,7 +51,7 @@ const SignIn = (props) => {
                 className="gx-signin-form gx-form-row0"
               >
                 <Form.Item
-                  name="email"
+                  name="emailId"
                   label="Email"
                   initialValue="pasala.kk@gmail.com"
                   rules={[
