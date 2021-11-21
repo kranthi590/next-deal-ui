46ff4613fe06b6ea254be15bc6ca6f1922d2dd4c
Anji <anji@Kranthis-MacBook-Pro.local>
2021-11-21 12:55:04 -0300

    base path

--- app/components/NextDeal/QuotationCard/index.js
+++ app/components/NextDeal/QuotationCard/index.js
@@ -14,17 +14,15 @@ const QuotationCard = () => {
 
   return (
     <>
-      <Widget
-        title={<h2 className="h4 gx-mb-1 gx-text-capitalize gx-show-hand" onClick={toggleCustomizer}>
-          Wall paint for whole building</h2>
-        }>
-        <div className="gx-currentplan-row">
+      <Widget styleName={`gx-card-full gx-px-3`}>
+        <h2 className="h3 gx-mb-2 gx-pt-2 gx-show-hand" onClick={toggleCustomizer}>Interlocking bricks house construction</h2>
+        <div className="gx-currentplan-row gx-card-full">
           <div className="gx-currentplan-col"><h2
             className="gx-text-primary gx-fs-xlxl gx-font-weight-medium gx-mb-1">19<sub
             className="gx-fs-md gx-bottom-0">/Responses</sub></h2>
             <p className="gx-mb-1">
-            <span className="gx-size-8 gx-bg-dark gx-rounded-xs gx-d-inline-block gx-mr-1"/> Lorem ipsum - 10
-          </p>
+              <span className="gx-size-8 gx-bg-dark gx-rounded-xs gx-d-inline-block gx-mr-1"/> Lorem ipsum - 10
+            </p>
             <p>
               <span className="gx-size-8 gx-bg-dark gx-rounded-xs gx-d-inline-block gx-mr-1"/> Lorem ipsum - 10
             </p>
@@ -34,7 +32,7 @@ const QuotationCard = () => {
               className="gx-fs-md gx-bottom-0">/Suppliers</sub></h2>
             <div className="gx-currentplan-right-content">
             <span className="gx-text-primary gx-fs-md gx-pointer gx-mts-3 gx-oth-plans-down">Other plans <i
-  className="icon icon-long-arrow-right gx-fs-xxl gx-ml-2 gx-d-inline-flex gx-vertical-align-middle"/></span>
+              className="icon icon-long-arrow-right gx-fs-xxl gx-ml-2 gx-d-inline-flex gx-vertical-align-middle"/></span>
             </div>
           </div>
         </div>
