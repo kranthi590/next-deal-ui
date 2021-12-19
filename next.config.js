812bec32ddb8604cc2bafd2ab1d6126afbfcd40e
Harish Pagadimarri <h0p02fc@m-C02DW09MMD6Q.Dlink>
2021-12-19 17:50:55 +0530

    bash for shell scripts

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config({path: require('find-config')('.env')});
 const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css');
 
