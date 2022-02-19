a1746b98acdc3bb01f409606f7a9db9bf7ab8db0
kranthi kumar pasala <kpasala@gap.com>
2022-02-19 16:13:26 +0530

    add token to url

--- app/common/FileManager/index.js
+++ app/common/FileManager/index.js
@@ -55,7 +55,7 @@ export default class PicturesWall extends React.Component {
   handleChange = ({ fileList}) => {
     fileList.forEach((file) => {
       if (!file.url && file.response) {
-        file.url = file.response.data[0].fileUrl;
+        file.url = `${file.response.data[0].fileUrl}?token=${cookie.get('token')}`;
       }
     });
     this.setState({ fileList });
